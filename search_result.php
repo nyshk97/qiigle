3f60ff3ad895ad18b31ba758ac245e3ddf6b1446
tsubasa namatame <nyshk97@gmail.com>
2020-08-01 15:45:13 +0900

    API叩いた結果を変数に保存

--- search_result.php
+++ search_result.php
@@ -2,28 +2,29 @@
 if($_GET['user']) {
   $q_user = urlencode("user:{$_GET['user']} ");
 }
-
 if($_GET['title']) {
   $q_title = urlencode("title:{$_GET['title']} ");
 }
-
 if($_GET['body']) {
   $q_body = urlencode("body:{$_GET['body']} ");
 }
-
 if($_GET['tag']) {
   $q_tag = urlencode("tag:{$_GET['tag']} ");
 }
 
-
 $base_url = "https://qiita.com/api/v2/items";
 $url = "{$base_url}?query={$q_user}{$q_title}{$q_body}{$q_tag}";
 
-print $url;
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-$res =  curl_exec($ch);
-var_dump($res);
-curl_close($conn);
+$curl = curl_init();
+$option = [
+  CURLOPT_URL => $url,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_RETURNTRANSFER => true,
+];
+curl_setopt_array($curl, $option);
+$response = curl_exec($curl);
+$result = json_decode($response, true);
+curl_close($curl);
+print $result[0]['title']
 ?>
